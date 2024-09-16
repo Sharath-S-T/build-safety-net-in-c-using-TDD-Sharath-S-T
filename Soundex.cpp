3a53d820e3f6ce940f49a9b2c5c83a8197311fbf
Sharath-S-T <sharath.st123@gmail.com>
2024-09-16 17:24:09 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -13,7 +13,7 @@ void generateSoundex(const char* input, char* soundex)
       char c = toupper(input[i]);
       if(c <= 'A' && c >= 'Z')
       {
-        soundex[0] = '\0'
+        soundex[0] = '\0';
         return ;
       }
     }
