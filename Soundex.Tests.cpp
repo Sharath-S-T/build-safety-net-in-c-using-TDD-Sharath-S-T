e9cbb0b5802b9f6343e9a719efa58901fff7b5d0
Sharath-S-T <sharath.st123@gmail.com>
2024-09-16 17:22:59 +0530

    added new test case in unittest

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -6,4 +6,7 @@ TEST(SoundexTestsuite, SoundexTestForEmptyString)
     char soundexCode[5];
     generateSoundex("",soundexCode);
     ASSERT_STREQ(soundexCode,"");
+
+    generateSoundex("1",soundexCode);
+    ASSERT_STREQ(soundexCode,"");
 }
