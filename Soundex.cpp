ebf60d232e3225423857d64191196ce2309906db
Sharath-S-T <sharath.st123@gmail.com>
2024-09-16 17:21:58 +0530

    added check if letter in betweeen a to z

--- Soundex.cpp
+++ Soundex.cpp
@@ -7,4 +7,14 @@ void generateSoundex(const char* input, char* soundex)
         soundex[0] = '\0';
         return;
     }
+
+  for(int index = 0; index< strlen(input);i++)
+    {
+      char c = toupper(input[i]);
+      if(c <= 'A' && c >= 'Z')
+      {
+        soundex[0] = '\0'
+        return ;
+      }
+    }
 }
