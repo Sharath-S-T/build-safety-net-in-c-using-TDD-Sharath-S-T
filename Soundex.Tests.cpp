394dc8255c82a0d5c30129e10260a96759ae43b3
Sharath-S-T <sharath.st123@gmail.com>
2024-09-16 14:09:10 +0530

    added a test case assert

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,2 +1,9 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
+
+TEST(SoundexTestsuite, SoundexTests)
+{
+    char soundexCode[5];
+    generateSoundex("",soundexCoode);
+    ASSERT_STREQ(soundexCode,"");
+}
