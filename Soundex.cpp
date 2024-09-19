a7fb7c7e7db82b2c38c919b89d4acb14b02fe7c1
Sharath-S-T <sharath.st123@gmail.com>
2024-09-19 09:59:43 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -25,10 +25,31 @@ void initializeSoundex(char firstChar, char *soundex ,int *sIndex)
     *sIndex = 1;
 }
 
+char getSoundexCode(char c)
+{
+    c = toupper(c);
+    static const char code[] = {'0','1','2','3','0','1','2','0','0','2','2','4','5','5','0','1','2','6','2','3','0','1','0','2','0','2'};
+    return (c >= 'A' && c <= 'Z') ? code[c - 'A'] : '0';
+}
+
+void processChars(const char* input,char* soundex,int* sIndex,int inputLength)
+{
+  for(int index = 1; index < inputLength ; index ++)
+  {
+    char code = getSoundexCode(input[index]);
+    if(code != '\0' && soundex[index-1]!= code)
+    {
+      soundex[(*sIndex)++] = code;
+    }
+  }
+}
+
 void generateSoundex(const char* input, char* soundex)
 {
     int sIndex = 0;
+    int inputLength = strlen(input);
     checkIfNull(input,soundex);
     initializeSoundex(input[0],soundex,&sIndex);
+    processChars(input,soundex,&sIndex,inputLength);
     padSoundex(soundex,&sIndex);
 }
