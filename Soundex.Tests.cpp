48d6bf23d8357702a59c5a8457cf85525e12d13f
Sharath-S-T <sharath.st123@gmail.com>
2024-09-19 08:34:40 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,9 +4,11 @@
 TEST(SoundexTestsuite, SoundexTestForEmptyString)
 {
     char soundexCode[5];
+    //Empty String
     generateSoundex("",soundexCode);
     ASSERT_STREQ(soundexCode,"");
 
+    //non alphabetic single character
     generateSoundex("1",soundexCode);
     ASSERT_STREQ(soundexCode,"");
 }
