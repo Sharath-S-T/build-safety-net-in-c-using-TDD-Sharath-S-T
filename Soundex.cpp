f1dfcc4da5cf883c7b2912b6d47b889012f45b9c
Sharath-S-T <sharath.st123@gmail.com>
2024-09-19 09:30:52 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -10,13 +10,25 @@ void checkIfNull(const char *name, char *soundex)
     }
 }
 
-void initializeSoundex(char firstChar, char *soundex)
+void padSoundex(char *soundex,int *sIndex)
+{
+    while (*sIndex < 4) 
+    {
+        soundex[(*sIndex)++] = '0';
+    }
+    soundex[4] = '\0';
+}
+
+void initializeSoundex(char firstChar, char *soundex ,int *sIndex)
 {
     soundex[0] = toupper(firstChar);
+    *sIndex = 1;
 }
 
 void generateSoundex(const char* input, char* soundex)
 {
-  checkIfNull(input,soundex);
-  initializeSoundex(input[0],soundex);
+    int sIndex = 0;
+    checkIfNull(input,soundex);
+    initializeSoundex(input[0],soundex,&sIndex);
+    padSoundex(soundex,&sIndex);
 }
